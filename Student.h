bc4edb5d078cf624c9e67ddc4d98bf10792de3ef
Anwarfares <149572793+Anwarfares@users.noreply.github.com>
2023-11-09 19:53:55 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,5 +1,12 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <string>
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
+class Course;
 
 class Student {
 
@@ -7,13 +14,13 @@ public:
     // Default constructor
     Student();
 
-
     // Constructor with ID, first name, last name, GPA and registered status
-    Student(const string& id, const string& firstname, const string& lastname, double gpa,bool reg,string major);
+    Student(const string& id, const string& firstname, const string& lastname, double gpa,string major);
     
     //Copy Constructor
     Student(const Student& other);
-    
+    //destructor
+    virtual~Student();
     // Getters
     string getfirstname() const;
     string getlastname()const;
@@ -31,12 +38,19 @@ public:
     void setreg(bool);
     void setmajor(string);
     
+    
     // Display student information
     void displayall(const Student&) const;
     void display(const Student&) const;
 
     friend ostream& operator<<(ostream& , const Student&);
-
+    
+    
+	//course related functions
+	void registercourse(Course* );
+	void displayregisteredcourses()const;
+	void dropcourse(Course*)const;
+	
 private:
     static int nbOfstds;
     string id;
@@ -45,10 +59,14 @@ private:
     double gpa;
     bool reg;
     string major;
+    string academicstatus;
+    //course class data fields
+    vector<Course*> registeredcourses;
+
 
     
 };
 
-#endif // STUDENT_H
+#endif 
 
     
