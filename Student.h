46612a2c0192818b6260549d958866b3f8f6d0ba
Anwarfares <149572793+Anwarfares@users.noreply.github.com>
2023-11-14 19:40:17 +0200

    Update Student.h //without commentation

--- Student.h
+++ Student.h
@@ -1,7 +1,9 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-#include <string>
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -15,42 +17,45 @@ public:
     Student();
 
     // Constructor with ID, first name, last name, GPA and registered status
-    Student(const string& id, const string& firstname, const string& lastname, double gpa,string major);
-    
-    //Copy Constructor
+    Student(const string& id, const string& firstname, const string& lastname,
+        double gpa, string major);
+
+    // Copy Constructor
     Student(const Student& other);
-    //destructor
-    virtual~Student();
+    // destructor
+    virtual ~Student();
     // Getters
     string getfirstname() const;
-    string getlastname()const;
+    string getlastname() const;
     string getid() const;
     double getGPA() const;
     string getAS() const;
-    bool isreg()const;
-    string getmajor()const;
-    //setters
-    void setfirstname(const string &);
-    void setlastname(const string &);
-    void setid(const string &);
+    bool isreg() const;
+    string getmajor() const;
+    // setters
+    void setfirstname(const string&);
+    void setlastname(const string&);
+    void setid(const string&);
     void setGPA(double);
     void setAS();
     void setreg(bool);
     void setmajor(string);
-    
-    
-    // Display student information
-    void displayall(const Student&) const;
-    void display(const Student&) const;
 
-    friend ostream& operator<<(ostream& , const Student&);
-    
-    
-	//course related functions
-	void registercourse(Course* );
-	void displayregisteredcourses()const;
-	void dropcourse(Course*)const;
-	
+    bool operator==(const Student& other);
+
+    // Display student information
+    ostream& displayall(ostream&) const;
+    ostream& display(ostream&) const;
+    ostream& displayStudentWithCourse(ostream& out) const;
+
+    friend ostream& operator<<(ostream&, const Student&);
+
+    // course related functions
+    void registercourse(Course*);
+    void displayregisteredcourses(ostream&) const;
+    void dropcourse(Course*);
+    vector<Course*> getregisteredcourse();
+
 private:
     static int nbOfstds;
     string id;
@@ -60,13 +65,10 @@ private:
     bool reg;
     string major;
     string academicstatus;
-    //course class data fields
+    // course class data fields
     vector<Course*> registeredcourses;
-
-
-    
 };
 
-#endif 
+istream& operator>>(istream&, Student&);
 
-    
+#endif
